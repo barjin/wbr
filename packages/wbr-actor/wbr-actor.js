0526a2143259cc20b19d94759d8415917523a226
barjin <jindrich.bar@gmail.com>
2021-12-20 20:06:30 +0100

    better package structure enforced

--- packages/wbr-actor/wbr-actor.js
+++ packages/wbr-actor/wbr-actor.js
@@ -1,8 +1,31 @@
 'use strict';
 const Interpret = require('@wbr-project/wbr-interpret').default;
 const Apify = require('apify');
+const {chromium} = require('playwright');
 
 Apify.main(async () => {
     const input = await Apify.getInput();
-    console.log(input);
+    
+    if(!input || !('workflow' in input)){
+        throw new SyntaxError('Missing workflow. Nothing to run, stopping...');
+    }
+
+    const dataset = await Apify.openDataset();
+    const kvs = await Apify.openKeyValueStore();
+
+    const interpreter = new Interpret(input.workflow, 
+    {
+      serializableCallback: (row) => dataset.pushData(row),
+      binaryCallback: (data, mimeType) => kvs.setValue(`${Date.now()}`, data, { contentType: mimeType }),
+    });
+
+    const browser = await chromium.launch(process.env.DOCKER
+        ? { executablePath: process.env.CHROMIUM_PATH, args: ['--no-sandbox', '--disable-gpu'] }
+        : { });
+  
+    const ctx = await browser.newContext({ locale: 'en-GB' });
+    const page = await ctx.newPage();
+
+    await interpreter.run(page, input.params);
+
 });
