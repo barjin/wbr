322e9944f850d79cd9f0a5e8e5dbc4fe51bc6b17
Jindřich Bär <jindrichbar@gmail.com>
2022-04-28 11:26:32 +0200

    feat: more detailed type export, version bump

--- packages/wbr-interpret/src/index.ts
+++ packages/wbr-interpret/src/index.ts
@@ -2,5 +2,7 @@ import Interpreter from './interpret';
 
 export default Interpreter;
 export { default as Preprocessor } from './preprocessor';
-export type { WorkflowFile } from './types/workflow';
+export type {
+  WorkflowFile, WhereWhatPair, Where, What,
+} from './types/workflow';
 export { unaryOperators, naryOperators, meta as metaOperators } from './types/logic';
