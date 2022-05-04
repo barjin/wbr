d8f0fec0fbe2a52b4c435890c77e07636e22c6d7
Jindřich Bär <jindrichbar@gmail.com>
2022-05-04 21:25:19 +0200

    feat: wbr-editor examples added

---
+++ packages/wbr-editor/src/examples/cdcz.waw.ts
@@ -0,0 +1,116 @@
+export default {
+  meta: {
+    name: 'CD.cz',
+    desc: 'Scraper for the Ceske Drahy online shop. It shows form handling, pagination, and schematic scraping capabilities.',
+  },
+  workflow: [
+    {
+      id: 'closePopups',
+      where: {
+        selectors: [
+          '#consentBtnall.btnallow',
+        ],
+      },
+      what: [
+        {
+          action: 'click',
+          args: ['#consentBtnall.btnallow'],
+        },
+        {
+          action: 'waitForLoadState',
+        },
+      ],
+    },
+    {
+      id: 'scrapeSearchResults',
+      where: {
+        url: {
+          $regex: '^https://www.cd.cz/spojeni-a-jizdenka/spojeni-tam/.*',
+        },
+      },
+      what: [
+        {
+          action: 'scrapeSchema',
+          args: [
+            {
+              from: '.res-city.rcheader + * > .res-cityname a',
+              to: '.res-city.res-bottom.rc2:last-of-type > .res-cityname a',
+              departure: '.res-city.rcheader + * > .res-inf',
+              departureTime: '.res-city.rcheader + * > .res-time',
+              arrival: '.res-city.res-bottom.rc2:last-of-type > .res-inf',
+              arrivalTime: '.res-city.res-bottom.rc2:last-of-type > .res-time',
+              price: '.buybut.green',
+            },
+          ],
+        },
+        {
+          action: 'click',
+          args: [
+            '.link-nxt',
+          ],
+        },
+        {
+          action: 'waitForLoadState',
+        },
+      ],
+    },
+    {
+      id: 'fillTheForm',
+      where: {
+        selectors: [
+          "[placeholder='Zadejte stanici odkud']",
+          "[placeholder='Zadejte stanici kam']",
+        ],
+      },
+      what: [
+        {
+          action: 'waitForLoadState',
+        },
+        {
+          action: 'fill',
+          args: [
+            "[placeholder='Zadejte stanici odkud']",
+            'Praha',
+          ],
+        },
+        {
+          action: 'keyboard.press',
+          args: [
+            'Enter',
+          ],
+        },
+        {
+          action: 'fill',
+          args: [
+            "[placeholder='Zadejte stanici kam']",
+            'Brno',
+          ],
+        },
+        {
+          action: 'keyboard.press',
+          args: [
+            'Enter',
+          ],
+        },
+        {
+          action: 'waitForNavigation',
+        },
+      ],
+    },
+    {
+      id: 'base',
+      where: {
+      },
+      what: [
+        {
+          action: 'goto',
+          args: ['https://www.cd.cz/spojeni-a-jizdenka/'],
+        },
+        {
+          action: 'waitForTimeout',
+          args: [2000],
+        },
+      ],
+    },
+  ],
+};
